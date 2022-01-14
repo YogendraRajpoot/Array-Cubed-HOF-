2dbd60f2e09e5208dc4347f66f6440b21f65a55f
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-01-15 02:45:51 +0530

    first commit

---
+++ AC.js
@@ -0,0 +1,18 @@
+// Given an array of numbers print the cube of those numbers (map)
+
+const arr = [1,2,3,4,5,6,7]
+
+// step=3 =>
+// const arrayCubed = x => x*x*x
+
+// console.log(arr.map(arrayCubed))
+
+
+// step=2 =>
+// console.log(arr.map(function(x){
+//     return x*x*x
+// }))
+
+
+// step=3 =>
+console.log(arr.map(x => x*x*x))
